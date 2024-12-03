c7bcf43a7391f48baef3540106abea2b1146c895
Nawaf AlGhamdi <98660296+Nawaf9529@users.noreply.github.com>
2024-12-03 19:20:58 +0300

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,13 +3,12 @@ package org.example;
 import java.time.LocalDate;
 
 public class App{
-
+    //complete the method
     public int add(int x, int y){
-        return x+y;
-    }
 
+    }
+    
     public static void main(String[]args){
-        App app = new App();
-        app.add(2,3);
+
     }
 }
